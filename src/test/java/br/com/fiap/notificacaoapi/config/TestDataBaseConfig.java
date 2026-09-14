026f79bf1a329f869c3fc409908ef8dbbff02fff
ByGustavoo <132701636+ByGustavoo@users.noreply.github.com>
2026-09-13 23:38:24 -0300

    ADJ-1
    - Ajustando projeto

--- src/test/java/br/com/fiap/notificacaoapi/config/TestDataBaseConfig.java
+++ src/test/java/br/com/fiap/notificacaoapi/config/TestDataBaseConfig.java
@@ -16,13 +16,13 @@ public class TestDataBaseConfig {
     @Value("${DATABASE_USER:postgres}")
     private String usuario;
 
-    @Value("${DATABASE_PASSWORD:System@23}")
+    @Value("${DATABASE_PASSWORD:fiap@2026}")
     private String senha;
 
-    @Value("${DATABASE_PORT:5432}")
+    @Value("${DATABASE_PORT:8745}")
     private String porta;
 
-    @Value("${DATABASE_NAME:postgres2}")
+    @Value("${DATABASE_NAME:postgres}")
     private String nome;
 
     private String getUrl() {
